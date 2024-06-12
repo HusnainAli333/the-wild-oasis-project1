7372248ed151bbb0c31adb8043dc52751dfb30e0
HusnainAli333 <eyemusican333@gmail.com>
2024-06-12 09:24:26 +0500

    supa base done

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,7 +1,21 @@
+import { useEffect } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import { getCabins } from "../services/apiCabins";
 
 function Cabins() {
+  useEffect(() => {
+    async function fetchCabins() {
+      try {
+        const data = await getCabins();
+        console.log(data);
+      } catch (error) {
+        console.error("could not load the cabin data", error);
+      }
+    }
+
+    fetchCabins();
+  }, []);
   return (
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
